39bf2de45d550c97c5a916f68334d647fac6c0a7
STOKEBLOKE1 <56760932+STOKEBLOKE1@users.noreply.github.com>
2019-11-16 21:01:32 +0000

    fizzbuzz
    The most ubiquitous beginners coding challenge in the world

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 1000; i++)
+            {
+                if (i % 3 == 0 && i % 5 ==0)
+                {
+                    System.Console.WriteLine("FizzBuzz");
+                }
+                else if(i % 5 == 0)
+                {
+                    System.Console.WriteLine("Buzz");
+                }
+                else if(i % 3 == 0)
+                {
+                    System.Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    System.Console.WriteLine(i);
+                }
+
+            }
+            
+        }
+    }
+}
