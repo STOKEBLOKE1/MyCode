d914251e011f2f702a5cc80833d7e51c0d4a0939
STOKEBLOKE1 <56760932+STOKEBLOKE1@users.noreply.github.com>
2020-01-20 11:49:22 +0000

    Create SeiveOfEratosthenes.cs

---
+++ SeiveOfEratosthenes/SeiveOfEratosthenes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Primenumbers
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            List<int> NoPrime = new List<int>();
+
+            for (int x = 2; x < 1000; x++)
+            {
+                for (int y = x * 2; y < 1000; y = y + x)
+                {
+                    if (!NoPrime.Contains(y))
+                    {
+                        NoPrime.Add(y);
+                    }
+                }
+            }
+
+            for (int z = 2; z < 1000; z++)
+            {
+                if (!NoPrime.Contains(z))
+                {
+                    Console.WriteLine(z);
+                }
+            }
+        }
+    }
+}
